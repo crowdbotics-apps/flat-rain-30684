8041d483f4f35afc2d5495078e5b3bb43f6dcf90
Crowdbotics <team@crowdbotics.com>
2021-09-19 17:20:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "flat_rain_30684Identifier",
+  url: "https://flat_rain_30684Identifier.botics.co",
+  api: "https://flat_rain_30684Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
